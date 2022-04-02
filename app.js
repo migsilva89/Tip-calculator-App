f993e2b2623089fc1ee717500dbc27a03d299df5
migsilva89 <migdrum@gmail.com>
2022-04-02 03:52:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+function calcula() {
+    billValue = document.getElementById("billvalue")
+    customValue = document.getElementById("range")
+    var tipValue = null
+
+    if (customValue.value > 0){
+        tipValue = customValue.value
+    } else {
+        var radioValues = document.getElementsByName("radio")
+        for (var i = 0; i < radioValues.length; i++){
+            if(radioValues[i].checked == true) {
+                tipValue = radioValues[i].value
+            }
+        }  
+    }
+
+    numberOfPeople = document.getElementById("peoplenumber")
+    var totalTip = (billValue.value * tipValue) / 100
+    var tipPerPerson = totalTip / numberOfPeople.value
+    var billPerPerson = billValue.value / numberOfPeople.value
+    var totalPerPerson = tipPerPerson + billPerPerson
+    // console.log(totalPerPerson)
+}
---
+++ notes.txt
@@ -0,0 +1,19 @@
+  Bill
+
+  Select Tip %
+  5%
+  10%
+  15%
+  25%
+  50%
+  Custom
+
+  Number of People
+
+  Tip Amount
+  / person
+
+  Total
+  / person
+
+  Reset
